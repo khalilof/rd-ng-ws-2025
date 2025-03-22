6e8e23ab8d54505a1b6989e0a3eb6a4fc5d0b48c
khalilof <khalilof@gmail.com>
2025-03-22 22:04:00 +0100

    basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { WatchListComponent } from './watch-list/watch-list.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'watch-list',
+        component: WatchListComponent,
+    }
+];
